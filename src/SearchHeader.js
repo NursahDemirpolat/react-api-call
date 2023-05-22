f0187e67ad71842a58dbcbc06237f54c1e5fa50b
NursahDemirpolat <nursahdmrplt@gmail.com>
2023-05-22 18:53:13 +0300

    search header yapma

---
+++ src/SearchHeader.js
@@ -0,0 +1,12 @@
+function SearchHeader() {
+    return (
+        <div className="searchDiv">
+            <from className="seacrhFrom">
+                <label>Ne Arıyorsunuz?</label>
+                <input type="text"/>
+            </from>
+        </div>
+    )
+}
+
+export default SearchHeader;
